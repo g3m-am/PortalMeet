f55ab59def8d9e5eb486cc0c4abb97f6a4e6809e
andrzej morawski <andrzej.morawski@g3m.pl>
2019-09-24 09:57:08 +0200

    Utworzenie pierwszego modelu

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PortalMeet.API.Models;
+
+namespace PortalMeet.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        
+        public DbSet<Value> Values { get; set;}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using PortalMeet.API.Data;
 
 namespace PortalMeet.API
 {
@@ -20,11 +15,14 @@ namespace PortalMeet.API
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+            public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite("Conectionstring"));
+        
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,4 +39,6 @@ namespace PortalMeet.API
             app.UseMvc();
         }
     }
+    
+
 }
